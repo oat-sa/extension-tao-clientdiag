3158b2798ce37dba502a3f0536e4f802ab2d65fe
antoinerobin <antoine.robin@vesperiagroup.com>
2015-08-05 13:29:00 +0200

    grant compatibilitychecker access to anonymous role

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -42,6 +42,16 @@ class Updater extends \common_ext_ExtensionUpdater
             $currentVersion = '1.1.0';
         }
 
+        if ($currentVersion == '1.1.0') {
+            $accessService = \funcAcl_models_classes_AccessService::singleton();
+            $anonymous = new \core_kernel_classes_Resource('http://www.tao.lu/Ontologies/generis.rdf#AnonymousRole');
+            $accessService->grantModuleAccess($anonymous, 'taoClientDiagnostic', 'CompatibilityChecker');
+
+            $currentVersion = '1.1.1';
+        }
+
+
+
 		return $currentVersion;
 	}
 }
