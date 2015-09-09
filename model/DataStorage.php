678124a7cd7ca9abba863f19bd19ef22a5249264
antoinerobin <antoine.robin@vesperiagroup.com>
2015-09-09 11:30:51 +0200

    save info in atomic files in order to avoid file manipulation issues

--- model/DataStorage.php
+++ model/DataStorage.php
@@ -54,89 +54,45 @@ class DataStorage {
 
     function __construct($data)
     {
+        $this->key = 'anonymous';
         if(isset($data['key'])){
             $this->key = $data['key'];
         }
         $this->data = $data;
 
         $dataPath = FILES_PATH . 'taoClientDiagnostic' . DIRECTORY_SEPARATOR. 'storage' . DIRECTORY_SEPARATOR;
-        $this->filePath = $dataPath.'store.csv';
+        $this->filePath = $dataPath.'store_atomic_'. $this->key .'.csv';
     }
 
     public function storeData(){
-        if(!file_exists($this->filePath)){
-            $handle = fopen($this->filePath, 'w');
-            fputcsv($handle, array_keys($this->dataList),';');
-            fclose($handle);
-        }
 
         if(!is_null($this->isCompatible)){
             $this->data['compatible'] = (int)$this->isCompatible;
         }
 
-        if(!is_null($this->key)){
-            $data = $this->getStoredData();
-            if(is_array($data)){
-                $this->data = array_merge($data, $this->data);
-                $this->deleteData();
-            }
-            else{
-                $this->data = array_merge($this->dataList, $this->data);
-            }
+        $data = $this->getStoredData();
+        if(is_array($data)){
+            $this->data = array_merge($data, $this->data);
         }
-        $handle = fopen($this->filePath, 'a');
+        else{
+            $this->data = array_merge($this->dataList, $this->data);
+        }
+        $handle = fopen($this->filePath, 'w');
         fputcsv($handle, $this->data ,';');
         return fclose($handle);
     }
 
     public function getStoredData(){
-        if (($handle = fopen($this->filePath, "r")) !== FALSE) {
-            $line = 1;
-            $index = 0;
-            $keys = array();
+        if (file_exists($this->filePath) && ($handle = fopen($this->filePath, "r")) !== FALSE) {
+            $keys = array_keys($this->dataList);
             $returnValue = array();
-            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-                if($line === 1){
-                    $keys = $data;
-                    if(($index = array_search('key', $keys, true)) === false){
-                        return false;
-                    }
+            while (($data = fgetcsv($handle, 0, ";")) !== FALSE) {
+                foreach($data as $index => $value){
+                    $returnValue[$keys[$index]] = $value;
                 }
-                if($data[$index] === $this->key){
-                    foreach($data as $index => $value){
-                        $returnValue[$keys[$index]] = $value;
-                    }
-                    fclose($handle);
-                    return $returnValue;
-                }
-                $line++;
             }
             fclose($handle);
-        }
-        return false;
-    }
-
-    public function deleteData(){
-        if (($handle = fopen($this->filePath, "r")) !== FALSE) {
-            $tmpFile = \tao_helpers_File::createTempDir().'store.csv';
-            $tmpHandle = fopen($tmpFile,'w');
-            $line = 1;
-            $index = 0;
-            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-                if($line === 1){
-                    $keys = $data;
-                    if(($index = array_search('key', $keys, true)) === false){
-                        return false;
-                    }
-                }
-                if($data[$index] !== $this->key){
-                    fputcsv($tmpHandle, $data, ';');
-                }
-                $line++;
-            }
-            fclose($tmpHandle);
-            fclose($handle);
-            return \tao_helpers_File::copy($tmpFile, $this->filePath);
+            return $returnValue;
         }
         return false;
     }
