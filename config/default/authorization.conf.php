4badfe68cac421f3ae5a3fe76897c1616fae64a0
Moyon Camille <camille.moyon@vesperiagroup.com>
2016-02-11 14:42:22 +0100

    TAO-2196: set default authentication setting to anonymous

--- config/default/authorization.conf.php
+++ config/default/authorization.conf.php
@@ -1,12 +1,13 @@
 <?php
-/**
- * To display username field to authenticate (in ACL or matching with pattern)
- */
-return new oat\taoClientDiagnostic\model\authorization\RequireUsername(array(
-    'regexValidator' => '/^[0-9]{7}[A-Z]$/'
-));
 
 /**
  * For anonymous login
  */
-//return new oat\taoClientDiagnostic\model\authorization\Anonymous();
+return new oat\taoClientDiagnostic\model\authorization\Anonymous();
+
+/**
+ * To display username field to authenticate (in ACL or matching with pattern)
+ */
+//return new oat\taoClientDiagnostic\model\authorization\RequireUsername(array(
+//    'regexValidator' => '/^[0-9]{7}[A-Z]$/'
+//));
