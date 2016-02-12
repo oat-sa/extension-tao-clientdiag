4abf2ab82dbb966100202afc2fe036f3b1627601
Moyon Camille <camille.moyon@vesperiagroup.com>
2016-02-12 10:30:44 +0100

    Directory separator fix for template path

--- controller/Authenticator.php
+++ controller/Authenticator.php
@@ -60,6 +60,6 @@ class Authenticator extends \tao_actions_CommonModule
 
         $this->setData('clientConfigUrl', $this->getClientConfigUrl());
         $this->setData('successCallback', $this->getRequestParameter('successCallback'));
-        $this->setView('Authenticator\login.tpl');
+        $this->setView('Authenticator' . DIRECTORY_SEPARATOR . 'login.tpl');
     }
 }
