263778133ed608ce79c286d194c6e69c21b331df
jsconan <jsconan@gmail.com>
2016-02-22 16:32:50 +0100

    compile javascripts

--- views/js/controller/routes.js
+++ views/js/controller/routes.js
@@ -1,9 +1,12 @@
-define(function(){
-    return {
-        'CompatibilityChecker' : {
-            'actions' : {
-                'index' : 'controller/compatibilityChecker/index'
-            }
+define({
+    'Authenticator' : {
+        'actions' : {
+            'login' : 'controller/Authenticator/message'
         }
-    };
+    },
+    'CompatibilityChecker' : {
+        'actions' : {
+            'index' : 'controller/CompatibilityChecker/diagnostics'
+        }
+    }
 });
