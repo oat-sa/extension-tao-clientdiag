3aa32d68fd84cf943807d759b13e4afdfa93e447
Aleh Hutnikau <hutnikau@1pt.com>
2017-02-23 16:25:20 +0300

    add context_id column

--- views/js/controller/Diagnostic/diagnostic.js
+++ views/js/controller/Diagnostic/diagnostic.js
@@ -59,8 +59,8 @@ define([
             var $panel = $('.panel');
             var config = $container.data('config');
             var indexUrl = helpers._url('index', 'Diagnostic', extension);
-            var workstationUrl = helpers._url('workstation', 'DiagnosticChecker', 'taoClientDiagnostic');
-
+            var workstationUrl = helpers._url('workstation', 'DiagnosticChecker', extension);
+            console.log(config);
             /**
              * Installs the diagnostic tool GUI
              * @param {String} workstation
