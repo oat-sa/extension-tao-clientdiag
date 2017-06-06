84e811449f594924130bece4954df9a0d94cb145
Christophe Noël <christophe@taotesting.com>
2017-06-06 11:22:48 +0200

    added default value for configurableText

--- views/js/tools/diagnostic/diagnostic.js
+++ views/js/tools/diagnostic/diagnostic.js
@@ -52,7 +52,8 @@ define([
         actionStore: 'storeData',
         actionCheck: 'check',
         controller: 'DiagnosticChecker',
-        extension: 'taoClientDiagnostic'
+        extension: 'taoClientDiagnostic',
+        configurableText: {}
     };
 
     /**
