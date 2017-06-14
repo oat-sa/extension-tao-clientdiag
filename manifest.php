c37880cd8770604304009c8450fafb93d27f440f
Christophe Noël <christophe@taotesting.com>
2017-06-14 15:22:49 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -29,10 +29,10 @@ return array(
     'label'       => 'Browser and OS diagnostic tool',
     'description' => 'Check compatibility of the os and browser of a client',
     'license'     => 'GPL-2.0',
-    'version'     => '2.4.1',
+    'version'     => '2.5.0',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => array(
-        'tao'        => '>=10.18.0',
+        'tao'        => '>=10.21.0',
         'taoQtiItem' => '>=2.9',
     ),
     // for compatibility
