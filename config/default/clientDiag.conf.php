aa93fb3e218d5993a7050bd97040f81d01901a4b
bugalot <jerome@taotesting.com>
2018-01-10 13:17:04 +0100

    First trial.

--- config/default/clientDiag.conf.php
+++ config/default/clientDiag.conf.php
@@ -58,6 +58,14 @@ return array(
     'requireSchoolName' => false,
 
     /**
+     * An array describing how GET parameters in use when invoking the Diagnostic Tool should be transferred
+     * to the server side for storage. Keys describe the GET parameter names, and values describe the name of the
+     * data to be sent to the server side.
+     * @type array
+     */
+    'customInput' => [],
+
+    /**
      * List of testers that can be loaded
      * @type array
      */
@@ -320,6 +328,6 @@ return array(
              * @type string
              */
             'customMsgKey' => 'diagFingerprintCheckResult',
-        ],
+        ]
     ]
 );
