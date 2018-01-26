9c67a6421c70506fe37075c740f5345c46e79664
bugalot <jerome@taotesting.com>
2018-01-26 13:15:01 +0100

    licence issue

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'name'        => 'taoClientDiagnostic',
     'label'       => 'Browser and OS diagnostic tool',
     'description' => 'Check compatibility of the os and browser of a client',
-    'license'     => 'GPL-2.0-only',
+    'license'     => 'GPL-2.0',
     'version'     => '2.13.1',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => array(
