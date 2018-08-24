2b2003439132318d944c377091540ca1b65afaf4
Mikhail Kamarouski <kamarouski@1pt.com>
2018-08-24 14:50:46 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -713,7 +713,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('2.15.0');
         }
 
-        $this->skip('2.15.0', '2.17.6');
+        $this->skip('2.15.0', '2.17.7');
 
     }
 }
