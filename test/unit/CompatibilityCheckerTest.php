8832099b6ccdb0ea61fb7455c9e99b046b7ace83
siwane <camille@taotesting.com>
2018-12-20 16:18:03 +0100

    Refactor compatibility checker

--- test/unit/CompatibilityCheckerTest.php
+++ test/unit/CompatibilityCheckerTest.php
@@ -4,16 +4,16 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *  
+ *
  * Copyright (c) 2015 Open Assessment Technologies SA
  */
 
@@ -22,80 +22,95 @@
 namespace oat\taoClientDiagnostic\test\unit;
 
 use oat\taoClientDiagnostic\model\CompatibilityChecker;
+use Sinergi\BrowserDetector\Browser;
+use Sinergi\BrowserDetector\Os;
 
 class CompatibilityCheckerTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testCompatibleConfigTrue()
     {
-        $sentData = array(
-            'os'                => 'Windows',
-            'osVersion'         => '8.1',
-            'browser'           => 'Chrome',
-            'browserVersion'    => '33'
-        );
-
-        $checker = new CompatibilityChecker($sentData);
-
         $compatibility = array(
             (object) array("compatible" => 1, "os" => "Windows", "osVersion" => "8.1", "browser" => "Chrome", "versions" => array(33, 34, 35))
         );
-        $ref = new \ReflectionProperty('oat\taoClientDiagnostic\model\CompatibilityChecker', 'compatibility');
-        $ref->setAccessible(true);
-        $ref->setValue($checker, $compatibility);
 
-        $return = $checker->isCompatibleConfig();
+        $checker = new CompatibilityCheckerDummy(
+            $compatibility,
+            $this->getDetectorMock(Os::class, 'Windows', '8.1'),
+            $this->getDetectorMock(Browser::class, 'Chrome', '33')
+        );
 
-        // @todo return is actually 2
-        $this->assertEquals($return, 1);
-
+        $this->assertEquals(1, $checker->isCompatibleConfig());
     }
 
     public function testNotTestedCompatibleConfig()
     {
-        $sentData = array(
-            'os'                => 'Windows',
-            'osVersion'         => '8.1',
-            'browser'           => 'Chrome',
-            'browserVersion'    => 'NotVerison'
-        );
-
-        $checker = new CompatibilityChecker($sentData);
-
         $compatibility = array(
             (object) array("compatible" => 1, "os" => "Windows", "osVersion" => "8.1", "browser" => "Chrome", "versions" => array(33, 34, 35))
         );
-        $ref = new \ReflectionProperty('oat\taoClientDiagnostic\model\CompatibilityChecker', 'compatibility');
-        $ref->setAccessible(true);
-        $ref->setValue($checker, $compatibility);
 
-        $return = $checker->isCompatibleConfig();
+        $checker = new CompatibilityCheckerDummy(
+            $compatibility,
+            $this->getDetectorMock(Os::class, 'Windows', '8.1'),
+            $this->getDetectorMock(Browser::class, 'Chrome', 'NoVersion')
+        );
 
-        $this->assertEquals($return, 2);
+        $this->assertEquals(2, $checker->isCompatibleConfig());
     }
 
     public function testNotCompatibleConfig()
     {
-        $sentData = array(
-            'os'                => 'Windows',
-            'osVersion'         => '7',
-            'browser'           => 'Internet Explorer',
-            'browserVersion'    => '9'
-        );
-
-        $checker = new CompatibilityChecker($sentData);
-
         $compatibility = array(
             (object) array("compatible" => 0, "os" => "Windows", "osVersion" => "7", "browser" => "Internet Explorer", "versions" => array(9))
         );
-        $ref = new \ReflectionProperty('oat\taoClientDiagnostic\model\CompatibilityChecker', 'compatibility');
-        $ref->setAccessible(true);
-        $ref->setValue($checker, $compatibility);
 
-        $return = $checker->isCompatibleConfig();
+        $checker = new CompatibilityCheckerDummy(
+            $compatibility,
+            $this->getDetectorMock(Os::class, 'Windows', '7'),
+            $this->getDetectorMock(Browser::class, 'Internet Explorer', '9')
+        );
 
-        // @todo return is actually 2
-        $this->assertEquals($return, 0);
+        $this->assertEquals(0, $checker->isCompatibleConfig());
     }
 
+    protected function getDetectorMock($class, $name, $version)
+    {
+        $detector = $this->getMockBuilder($class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getName', 'getVersion'])
+            ->getMock();
+
+        $detector->expects($this->once())
+            ->method('getName')
+            ->willReturn($name);
+
+        $detector->expects($this->once())
+            ->method('getVersion')
+            ->willReturn($version);
+
+        return $detector;
+    }
+
+}
+
+class CompatibilityCheckerDummy extends CompatibilityChecker
+{
+    private $osDetector;
+    private $browserDetector;
+
+    public function __construct($compatibility, $osDetector, $browserDetector)
+    {
+        $this->compatibility = $compatibility;
+        $this->osDetector = $osDetector;
+        $this->browserDetector = $browserDetector;
+    }
+
+    protected function getBrowserDetector()
+    {
+        return $this->browserDetector;
+    }
+
+    public function getOsDetector()
+    {
+        return $this->osDetector;
+    }
 }
