3565e4d8e3fe099d4bec37387f62822cfbc820da
Anton Tsymuk <atsymuk@gmail.com>
2019-06-06 15:23:49 +0300

    fix Diagnostic controller to use nested config

--- views/js/controller/Diagnostic/index.js
+++ views/js/controller/Diagnostic/index.js
@@ -118,7 +118,8 @@ define([
             var extension = $container.data('extension') || 'taoClientDiagnostic';
             var $list = $container.find('.list');
             var dataset = $container.data('set');
-            var config = $container.data('config') || {};
+            var extensionConfig = $container.data('config') || {};
+            var config = extensionConfig.diagnostic || extensionConfig;
             var installedExtension = $container.data('installedextension') || false;
             var diagnosticUrl = helpers._url('diagnostic', 'Diagnostic', extension);
             var removeUrl = helpers._url('remove', 'Diagnostic', extension);
