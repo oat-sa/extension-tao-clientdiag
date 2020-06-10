3006913f0a570b72faa636c92b85db154fc0f802
gitromba <mihaly.hajdu@taotesting.com>
2020-06-10 12:00:02 +0200

    chore: bump manifest version

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return array(
     'label'       => 'Browser and OS diagnostic tool',
     'description' => 'Check compatibility of the os and browser of a client',
     'license'     => 'GPL-2.0',
-    'version'     => '7.5.0',
+    'version'     => '7.6.0',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => array(
         'generis'    => '>=12.15.0',
