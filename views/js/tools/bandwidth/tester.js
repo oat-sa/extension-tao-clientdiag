a0d9b4abd504e7356f74894d9548397d5976bbda
jsconan <jsconan@gmail.com>
2021-09-17 16:55:36 +0200

    refactor: convert tools/bandwidth to ES6

--- views/js/tools/bandwidth/tester.js
+++ views/js/tools/bandwidth/tester.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015-2017 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2015-2021 (original work) Open Assessment Technologies SA ;
  */
 define([
     'lodash',
@@ -30,50 +30,54 @@ define([
 
     /**
      * A binary kilo bytes (KiB)
-     * @type {Number}
+     * @type {number}
      * @private
      */
-    var _kilo = 1024;
+    const _kilo = 1024;
 
     /**
      * A binary mega bytes (MiB)
-     * @type {Number}
+     * @type {number}
      * @private
      */
-    var _mega = _kilo * _kilo;
+    const _mega = _kilo * _kilo;
 
     /**
      * Duration of one second (in milliseconds)
-     * @type {Number}
+     * @type {number}
      * @private
      */
-    var _second = 1000;
+    const _second = 1000;
 
     /**
      * A list of thresholds for bandwidth check
      * @type {Array}
      * @private
      */
-    var _thresholds = [{
-        threshold: 0,
-        message: __('Low bandwidth'),
-        type: 'error'
-    }, {
-        threshold: 33,
-        message: __('Average bandwidth'),
-        type: 'warning'
-    }, {
-        threshold: 66,
-        message: __('Good bandwidth'),
-        type: 'success'
-    }];
+    const _thresholds = [
+        {
+            threshold: 0,
+            message: __('Low bandwidth'),
+            type: 'error'
+        },
+        {
+            threshold: 33,
+            message: __('Average bandwidth'),
+            type: 'warning'
+        },
+        {
+            threshold: 66,
+            message: __('Good bandwidth'),
+            type: 'success'
+        }
+    ];
 
     /**
      * Default values for the bandwidth tester
-     * @type {Object}
+     * @type {object}
      * @private
      */
-    var _defaults = {
+    const _defaults = {
         id: 'bandwidth',
 
         // The typical bandwidth needed for a test taker (Mbps)
@@ -101,42 +105,43 @@ define([
      * - timeout : the timeout for the download
      * - threshold : a bandwidth threshold above which the data set can be downloaded to evaluate a more accurate value
      * - nb : number of download iterations
-     * @type {Object}
+     * @type {object}
+     * @private
      */
-    var _downloadData = {
-        "10KB" : {
-            id : '10KB',
-            file : 'data/bin10KB.data',
-            size : 10 * _kilo,
-            timeout : _second,
-            threshold : 0,
-            nb : 10
+    const _downloadData = {
+        '10KB': {
+            id: '10KB',
+            file: 'data/bin10KB.data',
+            size: 10 * _kilo,
+            timeout: _second,
+            threshold: 0,
+            nb: 10
         },
-        "100KB" : {
-            id : '100KB',
-            file : 'data/bin100KB.data',
-            size : 100 * _kilo,
-            timeout : 2 * _second,
-            threshold : 0,
-            nb : 5
+        '100KB': {
+            id: '100KB',
+            file: 'data/bin100KB.data',
+            size: 100 * _kilo,
+            timeout: 2 * _second,
+            threshold: 0,
+            nb: 5
         },
-        "1MB" : {
-            id : '1MB',
-            file : 'data/bin1MB.data',
-            size : _mega,
-            timeout : 20 * _second,
-            threshold : 0,
-            nb : 3
+        '1MB': {
+            id: '1MB',
+            file: 'data/bin1MB.data',
+            size: _mega,
+            timeout: 20 * _second,
+            threshold: 0,
+            nb: 3
         }
     };
 
     /**
      * List of translated texts per level.
      * The level is provided through the config as a numeric value, starting from 1.
-     * @type {Object}
+     * @type {object[]}
      * @private
      */
-    var _messages = [
+    const _messages = [
         // level 1
         {
             title: __('Bandwidth'),
@@ -159,83 +164,73 @@ define([
 
     /**
      * Download a data set as described by the provided descriptor and compute the duration.
-     * @param {Object} data The data set descriptor to use for download
+     * @param {object} data The data set descriptor to use for download
      * @param {Function} cb A callback function called at the end of the download.
      * This callback is also called if a timeout breaks the download;
+     * @private
      */
     function download(data, cb) {
-        var self = this;
-        var start, end;
-        var timeoutId;
-        var url;
-        var request;
-
         if (data.threshold && this.bandwidth < data.threshold) {
             return cb('threshold');
         }
 
-        url = context['root_url'] + 'taoClientDiagnostic/views/js/tools/bandwidth/' + data.file + '?' + Date.now();
-        request = new XMLHttpRequest();
+        const start = window.performance.now();
+        const url = `${context['root_url']}taoClientDiagnostic/views/js/tools/bandwidth/${data.file}?${Date.now()}`;
+        const timeoutId = window.setTimeout(cb, data.timeout, 'timeout');
+        const request = new XMLHttpRequest();
         request.open('GET', url, true);
         request.setRequestHeader('Accept', 'application/octet-stream');
 
-        request.onload = function onRequestLoad () {
-            var duration;
-            var bytes;
-            var seconds;
-            var speed;
-
-            end = window.performance.now();
+        request.onload = () => {
+            const end = window.performance.now();
             clearTimeout(timeoutId);
 
-            duration = end - start;
-            bytes = data.size;
-            seconds = duration / _second;
+            const duration = end - start;
+            const bytes = data.size;
+            const seconds = duration / _second;
 
             // speed in Mbps
-            speed = ((bytes * 8) / seconds) / _mega;
+            const speed = (bytes * 8) / seconds / _mega;
 
-            self.bandwidth = Math.max(self.bandwidth, speed);
+            this.bandwidth = Math.max(this.bandwidth, speed);
 
             return cb(null, {
-                id : data.id,
-                file : data.file,
-                size : data.size,
-                duration : duration,
-                speed : speed
+                id: data.id,
+                file: data.file,
+                size: data.size,
+                duration,
+                speed
             });
         };
-        request.onerror = function onRequestError (err) {
+        request.onerror = err => {
             clearTimeout(timeoutId);
             cb(err);
         };
 
-        request.onreadystatechange = function () {
-            if(request.readyState === 4 && request.status !== 200) {
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status !== 200) {
                 clearTimeout(timeoutId);
                 cb(request.status);
             }
         };
 
-        timeoutId = _.delay(cb, data.timeout, 'timeout');
-        start = window.performance.now();
         request.send();
     }
 
     /**
      * Performs a bandwidth test by downloading a bunch of data sets with different sizes
      *
-     * @param {Object} config - Some optional configs
-     * @param {String} [config.id] - The identifier of the test
-     * @param {Number} [config.unit] - The typical bandwidth needed for a test taker (Mbps)
-     * @param {Number} [config.ideal] - The thresholds for optimal bandwidth
-     * @param {Number} [config.max] - Maximum number of test takers to display
-     * @param {String} [config.level] - The intensity level of the test. It will aim which messages list to use.
-     * @returns {Object}
+     * @param {object} config - Some optional configs
+     * @param {string} [config.id] - The identifier of the test
+     * @param {number} [config.unit] - The typical bandwidth needed for a test taker (Mbps)
+     * @param {number} [config.ideal] - The thresholds for optimal bandwidth
+     * @param {number} [config.max] - Maximum number of test takers to display
+     * @param {string} [config.level] - The intensity level of the test. It will aim which messages list to use.
+     * @returns {object}
      */
-    function bandwidthTester (config) {
-        var initConfig = getConfig(config, _defaults);
-        var labels = getLabels(_messages, initConfig.level);
+    return function bandwidthTester(config) {
+        const initConfig = getConfig(config, _defaults);
+        const labels = getLabels(_messages, initConfig.level);
 
         // override the feedback thresholds given by the config in case it is an empty array
         if (_.isArray(initConfig.feedbackThresholds) && !initConfig.feedbackThresholds.length) {
@@ -247,31 +242,27 @@ define([
              * Performs a bandwidth test, then call a function to provide the result
              * @param {Function} done
              */
-            start : function start(done) {
-                var self = this;
-                var tests = [];
+            start(done) {
+                const tests = [];
 
-                _.forEach(_downloadData, function(data) {
-                    var cb = _.bind(download, self, data);
-                    var iterations = data.nb || 1;
-                    while (iterations --) {
+                _.forEach(_downloadData, data => {
+                    const cb = download.bind(this, data);
+                    let iterations = data.nb || 1;
+                    while (iterations--) {
                         tests.push(cb);
                     }
                 });
 
                 this.bandwidth = 0;
 
-                async.series(tests, function(err, measures) {
-                    var duration = 0;
-                    var size = 0;
-                    var decimals = 2;
-                    var results;
-                    var resultsBySize = {};
-                    var summary;
-                    var status;
+                async.series(tests, (err, measures) => {
+                    let duration = 0;
+                    let size = 0;
+                    const decimals = 2;
+                    const resultsBySize = {};
 
                     function getValue(value) {
-                        var speed = 0;
+                        let speed = 0;
 
                         if (value) {
                             duration += value.duration;
@@ -288,19 +279,21 @@ define([
                         //something went wrong
                         throw err;
                     }
-                    results = stats(measures, getValue, decimals);
+                    const results = stats(measures, getValue, decimals);
 
-                    _.forEach(_downloadData, function (data, key) {
-                        resultsBySize[key] = stats(_.filter(measures, function(o) {
-                            return o.id === key;
-                        }), getValue, decimals);
+                    _.forEach(_downloadData, (data, key) => {
+                        resultsBySize[key] = stats(
+                            _.filter(measures, o => o.id === key),
+                            getValue,
+                            decimals
+                        );
                     });
 
                     results.duration = fixedDecimals(duration / _second, decimals);
                     results.size = size;
 
-                    summary = self.getSummary(results);
-                    status = self.getFeedback(results, resultsBySize);
+                    const summary = this.getSummary(results);
+                    const status = this.getFeedback(results, resultsBySize);
 
                     done(status, summary, results);
                 });
@@ -308,7 +301,7 @@ define([
 
             /**
              * Gets the labels loaded for the tester
-             * @returns {Object}
+             * @returns {object}
              */
             get labels() {
                 return labels;
@@ -316,53 +309,47 @@ define([
 
             /**
              * Builds the results summary
-             * @param {Object} results
-             * @returns {Object}
+             * @param {object} results
+             * @returns {object}
              */
-            getSummary: function getSummary(results) {
+            getSummary(results) {
                 return {
-                    bandwidthMin: {message: labels.bandwidthMin, value: results.min + ' Mbps'},
-                    bandwidthMax: {message: labels.bandwidthMax, value: results.max + ' Mbps'},
-                    bandwidthAverage: {message:  labels.bandwidthAverage, value: results.average + ' Mbps'}
+                    bandwidthMin: { message: labels.bandwidthMin, value: results.min + ' Mbps' },
+                    bandwidthMax: { message: labels.bandwidthMax, value: results.max + ' Mbps' },
+                    bandwidthAverage: { message: labels.bandwidthAverage, value: results.average + ' Mbps' }
                 };
             },
 
             /**
              * Gets the feedback status for the provided result value
-             * @param {Object} result
-             * @param {Number} result.max
-             * @param {Number} result.min
-             * @param {Number} result.average
-             * @param {Number} resultsBySize - result statistics grouped by size
-             * @returns {Object}
+             * @param {object} result
+             * @param {number} result.max
+             * @param {number} result.min
+             * @param {number} result.average
+             * @param {object} resultsBySize - result statistics grouped by size
+             * @returns {object}
              */
-            getFeedback: function getFeedback(result, resultsBySize) {
-                var avgResult = result.average;
-                var bandwidthUnit = initConfig.unit;
-                var threshold = initConfig.ideal;
-                var maxTestTakers = initConfig.max;
-                var max = threshold * bandwidthUnit;
-                var getStatusOptions = (initConfig.minimumGlobalPercentage)
+            getFeedback(result, resultsBySize) {
+                const avgResult = result.average;
+                const bandwidthUnit = initConfig.unit;
+                const threshold = initConfig.ideal;
+                const maxTestTakers = initConfig.max;
+                const max = threshold * bandwidthUnit;
+                const getStatusOptions = initConfig.minimumGlobalPercentage
                     ? { minimumGlobalPercentage: initConfig.minimumGlobalPercentage }
                     : {};
-                var baseBandwidth = avgResult;
-                var status;
-                var nb;
-                var stable = true;
+                const baseBandwidth = avgResult;
+                let stable = true;
 
-                _.forEach(resultsBySize, function (resultBySize){
+                _.forEach(resultsBySize, resultBySize => {
                     if (resultBySize.min / resultBySize.average < initConfig.fallbackThreshold) {
                         stable = false;
                     }
                 });
 
-                status = getStatus(
-                    baseBandwidth / max * 100,
-                    initConfig.feedbackThresholds,
-                    getStatusOptions
-                );
+                const status = getStatus((baseBandwidth / max) * 100, initConfig.feedbackThresholds, getStatusOptions);
 
-                nb = Math.floor(baseBandwidth / bandwidthUnit );
+                let nb = Math.floor(baseBandwidth / bandwidthUnit);
 
                 if (nb > maxTestTakers) {
                     nb = '>' + maxTestTakers;
@@ -375,7 +362,9 @@ define([
 
                 if (!stable) {
                     status.feedback.type = 'warning';
-                    status.feedback.message = __('Unstable bandwidth, temporary fluctuations in connection speed may affect test taker experience.');
+                    status.feedback.message = __(
+                        'Unstable bandwidth, temporary fluctuations in connection speed may affect test taker experience.'
+                    );
                     status.feedback.legend = __('Simultaneous test takers under normal connection conditions.');
                 }
 
@@ -386,7 +375,5 @@ define([
                 return status;
             }
         };
-    }
-
-    return bandwidthTester;
+    };
 });
