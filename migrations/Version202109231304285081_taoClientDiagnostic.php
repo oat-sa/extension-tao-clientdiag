9930e9cf046c392c659d43883c9dc7a451ba85e8
jsconan <jsconan@gmail.com>
2021-09-23 17:10:58 +0200

    feat: add a migration script to install the CompatibilityChecker service

---
+++ migrations/Version202109231304285081_taoClientDiagnostic.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoClientDiagnostic\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\taoClientDiagnostic\model\CompatibilityChecker;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202109231304285081_taoClientDiagnostic extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Register the service CompatibilityChecker';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->getServiceManager()->register(CompatibilityChecker::SERVICE_ID, new CompatibilityChecker());
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->getServiceManager()->unregister(CompatibilityChecker::SERVICE_ID);
+    }
+}
