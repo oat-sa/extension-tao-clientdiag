73129ffa30a49e97c262540eb91ad6d6d2e14c0a
Héctor Arroyo <hector.arroyo@taotesting.com>
2023-04-27 17:13:54 +0200

    chore: Manually fix leftover PSR12 errors

--- config/header/storage.conf.php
+++ config/header/storage.conf.php
@@ -15,14 +15,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016 Open Assessment Technologies SA
- *
- * @phpcsSuppress
+ * Copyright (c) 2016 Open Assessment Technologies SA.
  */
 
 /**
  * This configuration is for set storage processes.
  *
+ * @phpcsSuppress
  *
  *  ### SQL ###
  *
