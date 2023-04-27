692c1c65dd137094b08bc2e403291f9f81458c14
Héctor Arroyo <hector.arroyo@taotesting.com>
2023-04-27 15:41:47 +0200

    chore: Manually fix leftover PSR12 errors

--- test/unit/CompatibilityCheckerTest.php
+++ test/unit/CompatibilityCheckerTest.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015-2021 Open Assessment Technologies SA
+ * Copyright (c) 2015-2023 Open Assessment Technologies SA
  */
 
 declare(strict_types=1);
